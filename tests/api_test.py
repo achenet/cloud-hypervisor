147ca3aafacd7e53bf0eef16fe3226ad41d556aa
Ariel Chenet <apchenet@gmail.com>
2025-11-11 19:18:57 +0100

    Hypervisor booting is now non-blocking

--- tests/api_test.py
+++ tests/api_test.py
@@ -1,11 +1,18 @@
 import logging
 import socket
 import os
+import requests
 
 def test_vm_start():
     logging.warning("testing vm start")
-    # how to make this call not blocking?
-    os.system('../target/debug/cloud-hypervisor --unix-socket /tmp/cloud-hypervisor.sock')
+    os.system('./target/debug/cloud-hypervisor --api-socket /tmp/cloud-hypervisor.sock &')
+
+    logging.warning("VMM started")
+
+    url = 'http://localhost/api/v1/vmm.ping'
+    r = requests.get(url)
+    logging.warning("response", r)
 
 
 
+# test_vm_start()
