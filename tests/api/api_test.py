3e8f1c07c4b3a919a077dd3dc3ab4d6bfa985d08
Ariel Chenet <apchenet@gmail.com>
2025-11-19 12:25:49 +0100

    Use requests_unixsocket lib

--- tests/api/api_test.py
+++ tests/api/api_test.py
@@ -1,7 +1,6 @@
 import logging
-import socket
 import os
-import requests
+import requests_unixsocket
 
 def test_vm_start():
     logging.warning("testing vm start")
@@ -9,10 +8,8 @@ def test_vm_start():
 
     logging.warning("VMM started")
 
-    url = 'http://localhost/api/v1/vmm.ping'
-    r = requests.get(url)
-    logging.warning("response", r)
+    session = requests_unixsocket.Session()
+    url = 'http+unix://%2Ftmp%2Fcloud-hypervisor.sock/api/v1/vmm.ping'
+    r = session.get(url)
+    logging.warning(f"response: {r.status_code} - {r.text}")
 
-
-
-# test_vm_start()
